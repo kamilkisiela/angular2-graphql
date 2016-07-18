d0070186d8773407f870e6f8e7183c0a84522d68
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-18 17:55:58 +0200

    Subject

--- watchQuery.ts
+++ watchQuery.ts
@@ -3,28 +3,34 @@ import {
 } from '@angular/core';
 
 import {
-  Apollo,
-  ApolloQueryPipe
+  Apollo
 } from 'angular2-apollo';
+
+import {
+  QueryObservable
+} from 'apollo-client/QueryManager';
+
+import {
+  Subject
+} from 'rxjs/Subject';
 
 @Component({
   selector: 'app',
-  pipes: [
-    ApolloQueryPipe
-  ],
   template: `
     <ul>
-      <li *ngFor="let post of data | async | apolloQuery: 'posts'"></li>
+      <li *ngFor="let post of posts"></li>
     </ul>
   `
 })
 class App {
+  tag: Subject<string>;
+  data: QueryObservable;
+  posts: any;
+
   constructor(
     private apollo: Apollo;
-  ) {}
-
-  ngOnInit() {
-    this.apollo.watchQuery({
+  ) {
+    this.data = this.apollo.watchQuery({
       query: gql`
         query getPosts($tag: String) {
           posts(tag: $tag) {
@@ -33,8 +39,18 @@ class App {
         }
       `,
       variables: {
-        tag: '1234'
+        tag: this.tag
       }
     });
+  }
+
+  ngOnInit() {
+    this.data.subscribe(({ data }) => {
+      this.posts = this.data.posts;
+    });
+  }
+
+  filterByTag(tag: string) {
+    this.tag.next(tag); // rebuilds the query
   }
 }
